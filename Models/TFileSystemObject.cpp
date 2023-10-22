c74c2226acb8aa67d443743f1b01acc9e1ac4ebe
sideshowbobgot <sideshowbobgot@gmail.com>
2023-10-22 18:33:41 +0300

    in progress

--- Models/TFileSystemObject.cpp
+++ Models/TFileSystemObject.cpp
@@ -1,1 +1,13 @@
 #include "TFileSystemObject.hpp"
+
+cppfuse::TFileSystemObject::TFileSystemObject(const std::string& name, mode_t mode)
+    : m_sName{name}, m_uMode{mode} {}
+
+mode_t cppfuse::TFileSystemObject::Mode() const { return m_uMode; }
+
+mode_t cppfuse::TFileSystemObject::Mode(mode_t mode) { m_uMode = mode; }
+
+const std::string& cppfuse::TFileSystemObject::Name() const { return m_sName; }
+
+void cppfuse::TFileSystemObject::Name(const std::string& name) { m_sName = name; }
+
--- Models/TFileSystemObject.hpp
+++ Models/TFileSystemObject.hpp
@@ -6,7 +6,17 @@
 namespace cppfuse {
 
 class TFileSystemObject {
+    public:
+    TFileSystemObject(const std::string& name, mode_t mode);
+    virtual ~TFileSystemObject()=default;
+    [[nodiscard]] virtual mode_t Mode() const;
+    virtual mode_t Mode(mode_t mode);
+    [[nodiscard]] virtual const std::string& Name() const;
+    virtual void Name(const std::string& name);
 
+    protected:
+    std::string m_sName;
+    mode_t m_uMode = 0;
 };
 
 }
