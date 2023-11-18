7ff5b7f74b693a18ad879445732f7f6e52b61312
sideshowbobgot <sideshowbobgot@gmail.com>
2023-11-18 17:30:59 +0200

    WriteReadFile test implemented

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -59,5 +59,5 @@ TEST_F(TFileSystemTestFixture, WriteReadFile) {
     std::ofstream(filePath.c_str()) << "information";
     std::string r;
     std::ifstream(filePath.c_str()) >> r;
-    EXPECT_TRUE(r == "information");
+    EXPECT_EQ(r.find_first_of("information"), 0);
 }
